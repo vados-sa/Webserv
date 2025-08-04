1422c79b37ca4b6e9e6cb43a2506cc0ad1cb7942
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-08-04 17:40:22 +0200

    feat: enhance ServerConfig class with new attributes: error_pages_config & client_max_body_size

--- include/ServerConfig.hpp
+++ include/ServerConfig.hpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <unordered_map>
 
 #include "LocationConfig.hpp"
 
@@ -11,17 +12,26 @@ class ServerConfig
 private:
     int listen_port;
     std::string host;
+    std::unordered_map<int, std::string> error_pages_config;
+    int client_max_body_size;
     std::vector<LocationConfig> locations;
 
 public:
     ServerConfig();
-    void addLocation(LocationConfig &locConfig);
+    void addLocation(LocationConfig &locConfig) { locations.push_back(locConfig); };
 
     int getPort() { return listen_port; };
     const std::string &getHost() { return host; };
+    const std::unordered_map<int, std::string> &getErrorPagesConfig() { return error_pages_config; }
+    const std::string &getErrorPage (int code) const;
+    int getMaxBodySize() { return client_max_body_size; };
+    //get locations? talvez entender o uso antes
 
     void setHost(std::string set) { host = set; };
     void setPort(int set) { listen_port = set; };
+    void setErrorPagesConfig(std::unordered_map<int, std::string> set) { error_pages_config = set; };
+    void setMaxBodySize(int set) { client_max_body_size = set; };
+    //set location? entender o uso
 };
 
 std::ostream &operator<<(std::ostream &os, ServerConfig &obj);
--- src/ServerConfig.cpp
+++ src/ServerConfig.cpp
@@ -1,9 +1,19 @@
 #include "ServerConfig.hpp"
 
-ServerConfig::ServerConfig() : listen_port(-1), host("") {}
+ServerConfig::ServerConfig() {};
+
+const std::string &ServerConfig::getErrorPage(int code) const {
+    std::unordered_map<int, std::string>::const_iterator it = error_pages_config.find(code);
+
+    if (it != error_pages_config.end())
+        return it->second;
+
+    static const std::string empty = "";
+    return (empty);
+};
 
 std::ostream &operator<<(std::ostream &os, ServerConfig &obj) {
     os << "SERVERCONFIG:\nListen port: " << obj.getPort()
         << "\nHost: " << obj.getHost() << std::endl;
     return os;
-}
+};
