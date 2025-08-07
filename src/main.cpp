db02f7be187debfe4a4432ac8b07b04ff7657c95
mariaserpa <iza-serpa@hotmail.com>
2025-08-07 21:15:25 +0200

    change how to handle error exit

--- src/main.cpp
+++ src/main.cpp
@@ -3,26 +3,32 @@
 
 int main(int ac, char *av[])
 {
-	if (ac != 2) {
-		std::cerr << "Usage: " << av[0] << " <config_file>\n";
+	try {
+		if (ac != 2) {
+			std::cerr << "Usage: " << av[0] << " <config_file>\n";
+			return 1;
+		}
+		// IMPLEMENTAR - Garantir que a extensão do config file é .config
+		Config config(av[1]); //dps error check ou sei la! fiz aqui pra testar o parse
+		std::cout << config << std::endl;
+
+		Server server;
+		Server::instance = &server;
+		if (!server.setupServer(8080)) {
+			std::cerr << "Failed to setup server.\n";
+			return 1; // Server Destructor is called
+		};
+
+		if (!server.run()) {
+			std::cerr << "Server failed.\n";
+			return 1;
+		};
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "\033[31mException: " << e.what() << "\033[0m" << std::endl;
 		return 1;
 	}
-    // IMPLEMENTAR - Garantir que a extensão do config file é .config
-    Config config(av[1]); //dps error check ou sei la! fiz aqui pra testar o parse
-    std::cout << config << std::endl;
-
-	Server server;
-	Server::instance = &server;
-	if (!server.setupServer(8080)) {
-		std::cerr << "Failed to setup server.\n";
-		return 1; // Server Destructor is called
-	};
-
-	if (!server.run()) {
-		std::cerr << "Server failed.\n";
-		return 1;
-	};
-
 	// transform all if statements in try/catch blocks
 	return 0;
 }
