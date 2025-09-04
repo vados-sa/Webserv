355e3024341173288171bc570f459b209e20b1ba
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-09-04 14:48:17 +0200

    feat: now using config/simple.conf as default config file and being able to run the program with no argument.

--- src/main.cpp
+++ src/main.cpp
@@ -9,23 +9,27 @@ int main(int ac, char *av[])
 {
 	signal(SIGINT, signal_handler);
 
-	try {
-		if (ac != 2) {
-			throw std::runtime_error("Usage: ./webserv <config_file>");
-		}
+    try
+    {
+        std::string configfile;
+        if (ac != 2) {
+            configfile = "config/simple.conf";
+        } else {
+            configfile = av[1];
+        }
 
-		Config config(av[1]); //dps error check ou sei la! fiz aqui pra testar o parse
-		// std::cout << config << std::endl;
+        Config config(configfile);
 
-		if (!config.setupServer()) {
-			throw std::runtime_error("Failed to set server up.");
-		};
+        // std::cout << config << std::endl;
+        if (!config.setupServer()) {
+            throw std::runtime_error("Failed to set server up.");
+        };
 
-		if (!config.run()) {
-			throw std::runtime_error("Server failed to run.");
-		};
-	}
-	catch (const std::exception &e)
+        if (!config.run()) {
+            throw std::runtime_error("Server failed to run.");
+        };
+    }
+    catch (const std::exception &e)
 	{
 		std::cerr << "\033[31m" << e.what() << "\033[0m" << std::endl;
 		return 1;
