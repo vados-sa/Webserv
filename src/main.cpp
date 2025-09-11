b2e15bdadfb8d062fa7c15f420a9be400feacf98
Natália Schardosim <natalia.schardosim@hotmail.com>
2025-09-11 22:11:36 +0200

    tests: files to run 42 tester

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ int main(int ac, char *av[])
     {
         std::string configfile;
         if (ac != 2) {
-            configfile = "config/simple.conf";
+            configfile = "config/42tester.conf";
         } else {
             configfile = av[1];
         }
